44585134b82331b1bcb9fe027ad8de94cb5146c5
kalyskin <kalysrysmendeev@gmail.com>
2019-01-13 19:48:48 +0600

    notification fix

--- functions/index.js
+++ functions/index.js
@@ -7,41 +7,33 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
-exports.sendNotification = functions.database.ref('/Friend_Requests/{pushId}')
-    .onWrite(event => {
-        const message = event.data.current.val();
-        const senderUid = message.from;
-        const receiverUid = message.to;
-        const promises = [];
+exports.sendNotification = functions.database.ref('/Notifications/{pushId}')
+    .onWrite((event,context) => {
+        console.log('event', event);
+        const message = event.after.val();
+        const receiverToken = message.receiverToken;
+        const body = message.body;
+        const title = message.title;
+        const pushId = context.params.pushId;
 
-        if (senderUid == receiverUid) {
-            //if sender is receiver, don't send notification
-            promises.push(event.data.current.ref.remove());
-            return Promise.all(promises);
-        }
+        
+        //const getReceiverUidPromise = admin.auth().getUser(receiverUid);
 
-        const getInstanceIdPromise = admin.database().ref(`/users/${receiverUid}/device_token`).once('value');
-        const getReceiverUidPromise = admin.auth().getUser(receiverUid);
+        const payload = {
+            notification: {
+                title: title,
+                body: body
+            }
+        };
 
-        return Promise.all([getInstanceIdPromise, getReceiverUidPromise]).then(results => {
-            const instanceId = results[0].val();
-            const receiver = results[1];
-            console.log('notifying ' + receiverUid + ' about ' + ' message ' + ' from ' + senderUid);
+        console.log("pushId:",pushId);
 
-            const payload = {
-                notification: {
-                    title: 'Title',
-                    body: 'You have notification',
-                    icon: 'default'
-                }
-            };
-
-            admin.messaging().sendToDevice(instanceId, payload)
-                .then(function (response) {
-                    console.log("Successfully sent message:", response);
-                })
-                .catch(function (error) {
-                    console.log("Error sending message:", error);
-                });
-        });
-});
+        admin.messaging().sendToDevice(receiverToken, payload)
+            .then(function (response) {
+                admin.database().ref(`/Notifications`).child(pushId).remove();
+                console.log("Successfully sent message:", response);
+            })
+            .catch(function (error) {
+                console.log("Error sending message:", error);
+            });
+    });
